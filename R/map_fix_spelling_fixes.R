879fb85bfa3a0843761800c3e1e60eff0ff82363
egouldo <egouldo@users.noreply.github.com>
2024-04-18 18:19:45 +1000

    #53 Functionalise map_fix_spelling_fixes - and expand for application on spp to recode - ignore spp to recode where there are multiple suggestions per spp. in veg_data - add write flag arg

--- R/map_fix_spelling_fixes.R
+++ R/map_fix_spelling_fixes.R
@@ -22,14 +22,23 @@ get_spp_fixes <- function(df, .ignore_suggestions = NULL, name_col = "species", 
     mutate(words = strsplit(!!sym(value_col), "\\W+") %>% 
              lengths) %>% # remove single word suggestions
     filter(words > 1, 
-           score > 0.5)
+           score > 0.5) # remove low confidence suggestions
   
   # remove suggestions we marked for ignoring, if exists
    if(!is.null(.ignore_suggestions)){
      out <-  filter(out,!(!!sym(value_col) %in% .ignore_suggestions))
    }
+  
+  # Remove replacements where there is more than one suggestion for the same species
+  # (These will need to be manually fixed)
+  out <- out %>% 
+    group_by(!!name_col) %>% 
+    filter(n() == 1) %>% 
+    ungroup()
       
-  out %>% get_named_list_from_df(., name_col, value_col)
+  out <- out %>% get_named_list_from_df(., name_col, value_col)
+  
+  out
   
 }
 
@@ -70,28 +79,54 @@ get_veg_file <- function(stage, system, type){
 
 # Execute Species Recoding
 
-latest_spelling_fixes <-
-  dir_info(here::here("outputs", "QA_reports", "suggested_fixes", "spelling_fixes")) %>% 
-  select(path, change_time) %>% 
-  filter(str_detect(path, ".csv")) %>%
-  mutate(system = str_extract(path, paste0(.system_list, collapse= "|")),
-         file_type = str_extract(path, paste0(c("Point", "Recruits"), collapse = "|")),
-         stage = map_chr(path, get_VEFMAP_stage)) %>% 
-  group_by(system, file_type, stage) %>% 
-  filter(change_time == last(change_time)) %>% # for each unique QA (system by points by stage), get the latest file
-  mutate(spp_fixes_data = map(path, ~readr::read_csv(.x)), 
-         spp_fixes_data = map(spp_fixes_data, get_spp_fixes, .ignore_suggestions, "species", "suggested_replacement"),
-         veg_data_file = pmap(.l = list(stage, system, file_type), .f = get_veg_file) %>% flatten_chr,
-         veg_data = map(.x = veg_data_file,
-                        ~ readr::read_csv(here::here( "data/raw_data/veg_data/", .x),
-                                          col_types = readr::cols(.default = 
-                                                                    readr::col_character()))
-         ),
-         veg_data_recoded = map2(.x = veg_data, .y = spp_fixes_data, 
-                                ~ recode_veg_data(.x, "SPECIES", .y))
-           
-  )
+# extract function from the code that creates object latest_spelling_fixes
+# to apply to the latest spelling fixes
 
-latest_spelling_fixes %>% 
+apply_veg_data_fixes_from_files <- function(type = c("spelling_fixes", "species_to_recode"), .ignore_suggestions = NULL, write = FALSE) {
+  
+  if(type == "spelling_fixes"){
+    error_col <- "species"
+    replacement_col <- "suggested_replacement"
+  } else if(type == "species_to_recode"){
+    error_col <- "species_veg_data"
+    replacement_col <- "species_master"
+  }
+  
+  latest_fixes <-
+    dir_info(here::here("outputs", "QA_reports", "suggested_fixes", !!type)) %>% 
+    select(path, change_time) %>% 
+    filter(str_detect(path, ".csv")) %>%
+    mutate(system = str_extract(path, paste0(.system_list, collapse= "|")),
+           file_type = str_extract(path, paste0(c("Point", "Recruits"), collapse = "|")),
+           stage = map_chr(path, get_VEFMAP_stage)) %>% 
+    group_by(system, file_type, stage) %>% 
+    filter(change_time == last(change_time)) %>% # for each unique QA (system by points by stage), get the latest file
+    mutate(spp_fixes_data = map(path, ~readr::read_csv(.x)), 
+           spp_fixes_data = map(spp_fixes_data, get_spp_fixes, .ignore_suggestions, error_col, "replacement_col"),
+           veg_data_file = pmap(.l = list(stage, system, file_type), .f = get_veg_file) %>% flatten_chr,
+           veg_data = map(.x = veg_data_file,
+                          ~ readr::read_csv(here::here( "data/raw_data/veg_data/", .x),
+                                            col_types = readr::cols(.default = 
+                                                                      readr::col_character()))
+           ),
+           veg_data_recoded = map2(.x = veg_data, .y = spp_fixes_data, 
+                                   ~ recode_veg_data(.x, "SPECIES", .y))
+           
+    )
+  
+  if(write == TRUE){
+    latest_fixes %>% 
+      pull(veg_data_recoded, veg_data_file) %>% 
+      walk2(.x = ., .y = names(.), ~ write_csv(x = .x, file = here::here("data", "raw_data", "veg_data", .y)))
+    
+    cli::cli_alert_info("Veg data files have been updated to file")
+  }
+  
+  latest_fixes
+}
+
+
+
+latest_fixes %>% 
   pull(veg_data_recoded, veg_data_file) %>% 
   walk2(.x = ., .y = names(.), ~ write_csv(x = .x, file = here::here("data", "raw_data", "veg_data", .y)))
