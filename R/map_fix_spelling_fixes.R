d58970b869113c70b472f317769cad665ef7acc8
egouldo <egouldo@users.noreply.github.com>
2024-04-18 15:50:00 +1000

    #53 complete map fix spelling errors - add map helper functions for creating species fixes named vector - add ma[ helper fucntion for recoding veg data - restructure funs/obj setup - apply recoding in map/dplyr approach - delete dev code - Note: not yet executed, draft script only

--- R/map_fix_spelling_fixes.R
+++ R/map_fix_spelling_fixes.R
@@ -8,16 +8,36 @@ library(fs)
 source("R/utils.R")
 source("R/data.R")
 
+# Define Helper Functions
+
 get_named_list_from_df <- function(df, name_col, value_col){
   df %>%
     select(!!name_col, !!value_col) %>%
     deframe()
 }
 
+get_spp_fixes <- function(df, .ignore_suggestions, name_col = "species", value_col = "suggested_replacement"){
+  
+  
+  spelling_fixes %>% 
+    mutate(words = strsplit(!!sym(value_col), "\\W+") %>% 
+             lengths) %>% # remove single word suggestions
+    filter(words > 1, 
+           score > 0.5, 
+           !(!!sym(value_col) %in% .ignore_suggestions)) %>%  # remove suggestions we marked for ignoring
+    get_named_list_from_df(., name_col, value_col)
+
+  
+}
+
+
+recode_veg_data <- function(df,col_to_recode, recode_table){
+  df %>% 
+    mutate(!!col_to_recode := recode(!!sym(col_to_recode), !!!recode_table))
+} 
+
 get_VEFMAP_stage <- . %>%  str_split("_") %>% flatten_chr() %>% keep(., str_detect(., "VEFMAPS"))
 
-.system_list <- c("Campaspe", "Wimmera", "Moorabool", "Loddon", "Yarra", "ThomsonMacalister", "Glenelg") %>%
-  set_names(.) #TODO remove once merged into master as appears on diff branch in R/data.R
 
 get_veg_file <- function(stage, system, type){
   
@@ -30,13 +50,18 @@ get_veg_file <- function(stage, system, type){
   filename
 }
 
-debug(get_veg_file)
+# Create Lookup Objects
 
+.system_list <- c("Campaspe", "Wimmera", "Moorabool", "Loddon", "Yarra", "ThomsonMacalister", "Glenelg") %>%
+  set_names(.) #TODO remove once merged into master as appears on diff branch in R/data.R
 
-# Load data
-veg_data <- readr::read_csv(here("data/raw_data/veg_data/VEFMAPS7_Moorabool_2022_2023_Point.csv"),
-                            col_types = readr::cols(.default = readr::col_character())) # to preserve erroneously entered text that needs to be manually fixed
+# suggested fixes we don't want to implement because there are matches in species master
+# or there are multiple matches in World Flora Bank and we manually picked the closest match.
+.ignore_suggestions <- c("Epilobium billardiereanum f. billardiereanum", 
+                         "Eucalyptus camaldulensis var. camaldulensis", 
+                         "Juncus articulatus var. articulatus")
 
+# Execute Species Recoding
 
 latest_spelling_fixes <-
   dir_info(here::here("outputs", "QA_reports", "suggested_fixes", "spelling_fixes")) %>% 
@@ -48,20 +73,22 @@ latest_spelling_fixes <-
   group_by(system, file_type, stage) %>% 
   filter(change_time == last(change_time)) %>% # for each unique QA (system by points by stage), get the latest file
   mutate(spp_fixes_data = map(path, ~readr::read_csv(.x) %>% filter(is.na(genus))), # remove spp that aren't in species master
-         spp_fixes_data = map(spp_fixes_data, ~get_named_list_from_df(.x, "species", "suggested_replacement")),
-         veg_data_file = pmap(.l = list(stage, system, file_type), .f = get_veg_file) %>% flatten_chr)
+         spp_fixes_data = map(spp_fixes_data, get_spp_fixes, .ignore_suggestions, "species", "suggested_replacement"),
+         veg_data_file = pmap(.l = list(stage, system, file_type), .f = get_veg_file) %>% flatten_chr,
+         veg_data = map2(.x = veg_data_file, .y = spp_fixes_data,
+                        ~ readr::read_csv(here::here( "data/raw_data/veg_data/", .x),
+                                          col_types = readr::cols(.default = 
+                                                                    readr::col_character())) %>% 
+                          recode_veg_data(., "SPECIES", .y)
+         )
+           
+  )
+
+
+# Don't forget to save the recoded veg data!
+
+# walk(latest_spelling_fixes, ~write_csv(.x$veg_data, here::here("data", "raw_data", "veg_data", .x$veg_data_file)))
 
 
 
-
-get_named_list_from_df(latest_spelling_fixes$data[[1]], "species", "suggested_replacement")
-
-
-.spelling_fixes <- 
-  spelling_fixes %>% 
-  filter(!is.na(genus)) %>% 
-  pull(suggested_replacement, species)
-
-veg_data %>% 
-  mutate(SPECIES = recode(SPECIES, !!!.spelling_fixes))
 
